6994a49d3919bfaf5267474adfa45d4f304d6ae9
bados422 <bados4223@icloud.com>
2024-06-09 09:58:38 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const name = "Joe";
-const height = 74;
-const message = expected ('Joe' to equal 'Susan') 
+const name = "Susan";  // Changed this line to set name equal to "Susan"
+const height = 30;  // Set height to a value less than 40
+const message = `${name} is ${height} inches tall`;  // This provides a meaningful message
 
-module.exports = {name, height, message};
+module.exports = { name, height, message };
